0d057de8a3ec44fcd8362381065bb92a44531f25
Andrew McPherson <andrewmcp333@gmail.com>
2015-01-17 21:20:49 -0500

    Pulled a list of members from the organization.

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
-var firebase = new Firebase("https://mocsarcade.firebaseio.com/games");
-
 $(document).ready(function()
 {
-    $("form").on("submit", function(event)
+    /*$("form").on("submit", function(event)
     {
         event.preventDefault();
         
         var repo = $(this).find("input").val();
         repo = repo.replace("github.com", "cdn.rawgit.com") + "/master/package.json"
         
-        console.log(repo);
         jQuery.get(repo, function(data)
         {
-            console.log(data);
+            console.log(repo, data);
         })
-    });
+    });*/
     
-    $.localScroll();
+    jQuery.get("https://api.github.com/orgs/bellhops/members", function(members)
+    {
+        for(var index in members)
+        {
+            var $member = $("<a>");
+                $member.attr("target", "_blank");
+                $member.attr("href", members[index].html_url);
+            var $member_image = $("<img>");
+                $member_image.attr("src", members[index].avatar_url);
+                $member.append($member_image)
+                $member.attr("class", "member");
+            $("#members").append($member);
+        }
+    })
 });
